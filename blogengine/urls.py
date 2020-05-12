a06d69631d857e7075b3118254047d9d2c888fc7
ankiwoong <ankiwoong@gmail.com>
2020-05-13 08:37:26 +0900

    Detailed inquiry URL

--- blogengine/urls.py
+++ blogengine/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRecentPosts),
+    path('', views.getPosts),
+    path('<int:selected_page>/', views.getPosts),
 ]
