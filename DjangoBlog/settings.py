c22a9f624193102ffa6ca54fed29a0e53e7cd773
ankiwoong <ankiwoong@gmail.com>
2020-05-12 16:10:45 +0900

    Modify settings file

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@hg0-%e=ywi#m9+!(#a1)uxh(_o6a7rogh6vyu1or-lxd#ffuw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+INSTALLED_APPS += [
+    'blogengine',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
