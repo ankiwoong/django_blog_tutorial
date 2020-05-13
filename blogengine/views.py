66a0f3894db019ba3b6a5d031d7cb4755a52570c
ankiwoong <ankiwoong@gmail.com>
2020-05-13 14:31:37 +0900

    EmptyPage

--- blogengine/views.py
+++ blogengine/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 from blogengine.models import Post
 
@@ -9,7 +9,11 @@ def getPosts(request, selected_page=1):
     posts = Post.objects.all().order_by('-pub_date')
     # Add pagination
     pages = Paginator(posts, 5)
-    returned_page = pages.page(selected_page)
+    # Get the specified page
+    try:
+        returned_page = pages.page(selected_page)
+    except EmptyPage:
+        returned_page = pages.page(pages.num_pages)
     # Display all the posts
     return render(request, 'posts.html', {'posts': returned_page.object_list})
 
