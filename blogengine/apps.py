766a85d12b31397f561591b9a438fd92970dbd33
ankiwoong <ankiwoong@gmail.com>
2020-05-13 13:45:45 +0900

    DB Error

--- blogengine/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlogengineConfig(AppConfig):
-    name = 'blogengine'
--- blogengine/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.getPosts),
-    path('<int:selected_page>/', views.getPosts),
-    path('<slug:postSlug>/', views.getPost),
-]
