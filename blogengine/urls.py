ccbda22ff2eef57e8292c11a99e3429b11d7bb4f
ankiwoong <ankiwoong@gmail.com>
2020-05-20 13:18:45 +0900

    Part 4 - URLconf

--- blogengine/urls.py
+++ blogengine/urls.py
@@ -6,7 +6,25 @@ from . import views
 appname = 'blogengine'
 
 urlpatterns = [
+    # Examples:
+    # url(r'^$', 'blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.foo.urls')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the next line to enable the admin:
+    path('admin/', include(admin.site.urls)),
+
+    # Home page
     path('', views.getPosts),
     path('<int:selected_page>/', views.getPosts),
-    re_path(r'^\d{4}/\d{1,2}/(?P<postSlug>[-a-zA-Z0-9]+)/?$', views.getPost),
+
+    # Blog posts
+    re_path(r'^\d{4}/\d{1,2}/(?P[-a-zA-Z0-9]+)/?$', views.getPost),
+
+    # Categories
+    path('categories/<slug:categorySlug>/', views.getCategory),
+    path('categories/<slug:categorySlug>/<int:selected_page>/', views.getCategory)
+
+    # Flat pages
+    path('', include('django.contrib.flatpages.urls')),
 ]
