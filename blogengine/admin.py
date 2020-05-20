e2f54c47278e09e7b49df04819343dbad3fefe56
ankiwoong <ankiwoong@gmail.com>
2020-05-20 12:48:17 +0900

    Part 4 - admin modify

--- blogengine/admin.py
+++ blogengine/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 
-from .models import Post
+from .models import Post, Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class CategoryToPostInline(admin.TabularInline):
+    model = Category.CategoryToPost
+    extra = 1
 
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     exclude = ('author',)
+    inlines = [CategoryToPostInline]
 
     def save_model(self, request, obj, form, change):
         obj.author = request.user
@@ -14,3 +24,4 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
