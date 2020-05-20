adb3c2bd7eae35698bbe5830e87a073398361014
ankiwoong <ankiwoong@gmail.com>
2020-05-20 13:06:58 +0900

    Part 4 - views modify

--- blogengine/views.py
+++ blogengine/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage
 
-from blogengine.models import Post
+from blogengine.models import Post, Category
 
 
 def getPosts(request, selected_page=1):
@@ -23,3 +23,23 @@ def getPost(request, postSlug):
     post = Post.objects.filter(slug=postSlug)
     # Display specified post
     return render(request, 'single.html', {'posts': post})
+
+
+def getCategory(request, categorySlug, selected_page=1):
+    # Get specified category
+    posts = Post.objects.all().order_by('-pub_date')
+    category_posts = []
+    for post in posts:
+        if post.categories.filter(slug=categorySlug):
+            category_posts.append(post)
+    # Add pagination
+    pages = Paginator(category_posts, 5)
+    # Get the category
+    category = Category.objects.filter(slug=categorySlug)[0]
+    # Get the specified page
+    try:
+        returned_page = pages.page(selected_page)
+    except EmptyPage:
+        returned_page = pages.page(pages.num_pages)
+    # Display all the posts
+    return render(request, 'category.html', {'posts': returned_page.object_list, 'page': returned_page, 'category': category})
